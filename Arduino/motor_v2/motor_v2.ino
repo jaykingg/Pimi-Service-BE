bf3d92d345b85666ee58059c74fd24365b5231c2
MOJUNSEO <mojunseo49@naver.com>
2017-10-01 21:59:54 +0900

    RC카, 아두이노 부분 최종본 1-전진 / 2-후진 / 3-좌회전 / 4-우회전 / 0-멈춤 / 5-정지 시리얼 통신으로 받는 데이터는 String Type임.

---
+++ Arduino/motor_v2/motor_v2.ino
@@ -0,0 +1,83 @@
+//pin 7,6,5,4 activation
+//motor1
+const int IN1 = 7; 
+const int IN2 = 6;
+
+//motor2
+const int IN3 = 5;
+const int IN4 = 4;
+
+void setup()
+{
+ //open port number 9600
+ Serial.begin(9600);
+ //setting pinMode from using  
+ pinMode(IN1,OUTPUT);
+ pinMode(IN2,OUTPUT);
+ pinMode(IN3,OUTPUT);
+ pinMode(IN4,OUTPUT);
+}
+
+void loop()
+{
+   //if it can communicate by Serial
+   if(Serial.available())
+   {
+    String value = Serial.readString();
+
+    //classify into value
+    if(value == "1") //Go straight
+    {
+     //just msg
+     Serial.println("Go up"); 
+     //real code for moving
+     digitalWrite(IN1,HIGH);
+     digitalWrite(IN2,LOW);
+     digitalWrite(IN3,HIGH);
+     digitalWrite(IN4,LOW);       
+    }
+    if(value == "2") //Go back
+    {
+     //just msg
+     Serial.println("Go down"); 
+     //real code for moving
+     digitalWrite(IN1,LOW);
+     digitalWrite(IN2,HIGH);
+     digitalWrite(IN3,LOW);
+     digitalWrite(IN4,HIGH);
+    }
+    if(value == "3") //Go left
+    {
+     //just msg
+     Serial.println("Go left");  
+     //real code for moving
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
+    }
+    if(value == "4") //Go right
+    {
+     //just msg
+     Serial.println("Go right"); 
+     //real code for moving
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, LOW);
+    }
+    if(value == "0") // Stop
+    {
+     //just msg
+     Serial.println("Go stop");
+     //real code for moving
+     digitalWrite(IN1, LOW);
+     digitalWrite(IN2, LOW);
+     digitalWrite(IN3, LOW);
+     digitalWrite(IN4, LOW);
+    }
+   }
+   delay(10); 
+}
+
+
